700e029c8b969d216e4c53019e3c220bbd3abf98
Nelliugska171 <63161605+Nelliugska171@users.noreply.github.com>
2020-11-03 12:56:31 +0100

    Update customer-data.js

--- src/data/customer-data.js
+++ src/data/customer-data.js
@@ -3,7 +3,7 @@ export default [
     title: '"Gran aplicación"',
     content:
       'Gracias por esta gran aplicación, es muy buena, la idea de un fondo de pantalla desde un satélite es muy original, gracias.',
-    customerName: 'Elen Chufe',
+    customerName: 'Elen .C',
     customerTitle: '',
     customerImage: 'https://placeimg.com/150/150/people'
   },
@@ -11,7 +11,7 @@ export default [
     title: '"va de perlas"',
     content:
       'Esta buena la app, por el momento anda todo bien y lo probé de un punto a otro y realmente se mueve a donde estoy.',
-    customerName: 'Elvis Tec',
+    customerName: 'Elvis .T',
     customerTitle: '',
     customerImage: 'https://placeimg.com/150/150/people'
   },
@@ -19,7 +19,7 @@ export default [
     title: '"rápido y fácil de usar"',
     content:
       'Tal y como deberia de ser una app, perfecta.',
-    customerName: 'Elsa Polindo',
+    customerName: 'Elsa .P',
     customerTitle: '',
     customerImage: 'https://placeimg.com/150/150/people'
   }
